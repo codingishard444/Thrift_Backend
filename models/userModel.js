1c405049c343f45dbf4dea925486dcc1612573e5
Bryant <bryanteffendi1234@gmail.com>
2025-04-28 10:25:47 +0700

    added models that need further revision

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    fullname:{
+        type: String
+    },
     email: {
         type: String,
         required: true,
@@ -8,6 +11,11 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female','kids'],
         required: true
     }
 });
